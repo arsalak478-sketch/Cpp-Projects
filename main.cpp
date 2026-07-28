375d3f9f5d686a883d8c03b8bad3c38b12529141
arsalak478-sketch <arsalak478@gmail.com>
2026-07-28 20:25:14 +0530

    Update main.cpp code

--- main.cpp
+++ main.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
-    cout <<"Arsala khan" << endl;
+    string name = "Arsala khan";
+    int age = 18;
+    char section = 'A';
+
+    cout << "--------MY PROFILE---------" << endl;
+    cout << "The name of student is " << name << endl;
+    cout << "The age of student is " << age << endl;
+    cout << "The section of student is " << section << endl;
     return 0;
 }

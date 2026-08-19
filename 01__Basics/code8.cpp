8e04152861ef5d1b3034fb6e51b18bad1e1c41f7
arsalak478-sketch <arsalak478@gmail.com>
2026-08-19 23:43:54 +0530

    Added new C++ programs

---
+++ 01__Basics/code8.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main () { 
+    int age = 1;
+    switch (age) {
+    case 1:
+    cout << "You are 18" << endl;
+    case 2:
+    cout << "You are 22" << endl;
+    return 0;
+    }
+}
---
+++ 01__Basics/tempCodeRunnerFile.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n = 10;
+    if (n % 2 == 0) {
+    cout << "Even number";
+     } else {
+    cout << "Odd number";
+     }
+    return 0;
+}

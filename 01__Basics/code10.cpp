cd4a662e9b2045921fd551611e9c18390ea93a64
arsalak478-sketch <arsalak478@gmail.com>
2026-08-25 21:02:01 +0530

    Added new C++ program

---
+++ 01__Basics/code10.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    int b;
+
+    cout <<"Enter the value of a\n";
+    cin >> a;
+
+    cout << "Enter the value of b\n";
+    cin >> b;
+    
+    if (a>b) {
+        cout <<"a is maximum value\n";
+
+    } else {
+        cout <<"b is maximum value\n";
+    }
+
+    return 0;
+}

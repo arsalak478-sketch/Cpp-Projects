ed4379477754af5b44a22c068b86a6c75164ba37
arsalak478-sketch <arsalak478@gmail.com>
2026-08-25 21:31:33 +0530

    Added new C++ program

---
+++ 01__Basics/code11.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 5, m = 4, o = 3, p = 2, r = 1;
+        cout << "The factorial of 5 is " << n*m*o*p*r  <<endl;
+    
+
+    return 0;
+}

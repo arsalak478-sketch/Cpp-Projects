3387a044f4f16c5b8270657e70388a26c46bd8eb
arsalak478-sketch <arsalak478@gmail.com>
2026-08-30 18:00:58 +0530

    Added new C++ program

---
+++ 01__Basics/code25.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+ // Bitwise Operators
+
+int main() {
+    
+    int a=6, b=10;
+    //Bitwise AND
+    int c=a&b;
+    cout << c << endl;
+    //Bitwise OR
+    int d=a|b;
+    cout << d << endl;
+    //Bitwise XOR
+    int e=a^b;
+    cout << e << endl;
+    
+    return 0;
+}

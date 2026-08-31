f8845815764fbaa848db1c89dc9c2bee8c233744
arsalak478-sketch <arsalak478@gmail.com>
2026-08-31 22:57:33 +0530

    Added new C++ program

---
+++ 02__Arrays/array_basics.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std; 
+int main() {
+    int marks[5] = {9, 8, 7, 6, 4};
+    char grade[5] = {'A', 'B', 'C', 'D', 'E'};
+
+    cout << "Marks of first student is "<< marks[0] << " and grade is " << grade[0] << endl;
+    cout << "Marks of second student is "<< marks[1] << " and grade is " << grade[1] << endl;
+    cout << "Marks of third student is "<< marks[2] << " and grade is " << grade[2] << endl;
+    cout << "Marks of fourth student is "<< marks[3] << " and grade is " << grade[3] << endl;
+    cout << "Marks of fifth student is "<< marks[4] << " and grade is " << grade[4] << endl;
+    return 0;
+}
